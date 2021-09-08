62124ad41d1a852df36e540be99aeef8bc3f2879
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2021-09-08 16:13:30 +0900

    add: uptime

--- app.py
+++ app.py
@@ -4,42 +4,47 @@ import json
 import sys
 import socket
 
+ROUTER_IP_ADDR = "192.168.255.1"
+URL = f"http://{ROUTER_IP_ADDR}/cgi-bin/meco_web_cgi"
+
 cookies = {
-    'LiT': 'U',
-    'LiU': '0',
-    'loginChk': 'N',
-    'mdd': '100',
-    'patches': '0',
-    'uid': 'aef00de0ffcfaeaeadc',
-    'language': 'ko',
+    "LiT": "U",
+    "LiU": "0",
+    "loginChk": "N",
+    "mdd": "100",
+    "patches": "0",
+    "uid": "aef00de0ffcfaeaeadc",
+    "language": "ko",
 }
 
 headers = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': 'text/plain, */*; q=0.01',
-    'Accept-Language': 'ko-KR,ko;q=0.9',
-    'Accept-Encoding': 'gzip, deflate',
-    'Host': '192.168.255.1',
-    'Origin': 'http://192.168.255.1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15',
-    'Connection': 'keep-alive',
-    'Referer': 'http://192.168.255.1/basic/3g.html?random=1631007152543',
-    'Content-Length': '21',
-    'X-Requested-With': 'XMLHttpRequest',
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Accept": "text/plain, */*; q=0.01",
+    "Accept-Language": "ko-KR,ko;q=0.9",
+    "Accept-Encoding": "gzip, deflate",
+    "Host": ROUTER_IP_ADDR,
+    "Origin": ROUTER_IP_ADDR,
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+    "Connection": "keep-alive",
+    "Referer": f"http://{ROUTER_IP_ADDR}/basic/3g.html?random=1631007152543",
+    "Content-Length": "21",
+    "X-Requested-With": "XMLHttpRequest",
 }
 
 def getHostname(i):
     if i["ip_addr"] == "0.0.0.0":
-        return f"{socket.gethostname()}"
-    return f"{i['hostname']}"
+        return socket.gethostname()
+    return i['hostname']
 
 if len(sys.argv) == 1:
-    response = requests.post('http://192.168.255.1/cgi-bin/meco_web_cgi', headers=headers, cookies=cookies, data={'page': 'getDataUsageInfo'})
+    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getDataUsageInfo"})
     print("🚀 Ecogate Router Data Usage Viewer\n")
     print(f"🔥 Data Usage: {int((json.loads(response.text))['lgdatainfo']['mdatause'])/1000} MB")
-    response = requests.post('http://192.168.255.1/cgi-bin/meco_web_cgi', headers=headers, cookies=cookies, data={'page': 'getLanInfo'})
+    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getLanInfo"})
     print(f"💻 Connected Hosts: ", end="")
     print(", ".join(map(getHostname, json.loads(response.text)["wifi2Ghz"]["conn_client"]["conn_list"])))
+    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getWWanInfo"})
+    print(f"⏰ Uptime: {json.loads(response.text)['info']['h']}h {json.loads(response.text)['info']['m']}m {json.loads(response.text)['info']['s']}s")
 
 else:
     if sys.argv[1] in ["-h", "--help"]:
