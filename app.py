84f606d281a92bcddb93bb3468ef4925eb11ca67
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2021-09-08 16:20:34 +0900

    docs: update doc

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ if len(sys.argv) == 1:
     # d 추가할것
 else:
     if sys.argv[1] in ["-h", "--help"]:
-        print("이 프로그램은 mobileeco 사에서 생산한 ecogate (LG U+ Mobile Router)\n휴대용 라우터의 데이터 사용량을 확인할 수 있는 프로그램입니다.\n")
+        print("이 프로그램은 mobileeco 사에서 생산한 ecogate (LG U+ Mobile Router)\n휴대용 라우터의 연결 정보를 확인할 수 있는 프로그램입니다.\n")
         print("made by Devleo\n\nhttps://github.com/d3vle0")
