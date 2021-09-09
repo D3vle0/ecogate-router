c2cd93122eb558145a7fbc923afdfb1c6af3b10b
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2021-09-10 08:37:40 +0900

    update: remove unnecessary headers and cookies

--- app.py
+++ app.py
@@ -13,30 +13,6 @@ ROUTER_IP_ADDR = "192.168.255.1"
 URL = f"http://{ROUTER_IP_ADDR}/cgi-bin/meco_web_cgi"
 load_dotenv(verbose=True)
 
-cookies = {
-    "LiT": "U",
-    "LiU": "0",
-    "loginChk": "N",
-    "mdd": "100",
-    "patches": "0",
-    "uid": "aef00de0ffcfaeaeadc",
-    "language": "ko",
-}
-
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Accept": "text/plain, */*; q=0.01",
-    "Accept-Language": "ko-KR,ko;q=0.9",
-    "Accept-Encoding": "gzip, deflate",
-    "Host": ROUTER_IP_ADDR,
-    "Origin": ROUTER_IP_ADDR,
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
-    "Connection": "keep-alive",
-    "Referer": f"http://{ROUTER_IP_ADDR}/basic/3g.html?random=1631007152543",
-    "Content-Length": "21",
-    "X-Requested-With": "XMLHttpRequest",
-}
-
 def getHostname(i):
     if i["ip_addr"] == "0.0.0.0" and i["mac_addr"] == os.getenv("MY_MAC_ADDR"):
         return socket.gethostname()
@@ -49,18 +25,18 @@ if len(sys.argv) == 1:
     except:
         print("❗️ Please connect to your mobile router.")
         sys.exit(0)
-    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getDataUsageInfo"})
+    response = requests.post(URL, data={"page": "getDataUsageInfo"})
     print(colored("🚀 Ecogate Router Info Viewer\n", "green"))
     print("===== Basic Information =====")
     print(f"🔥 Data Usage: {int((json.loads(response.text))['lgdatainfo']['mdatause'])/1000} MB")
-    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getLanInfo"})
+    response = requests.post(URL, data={"page": "getLanInfo"})
     macAddr = json.loads(response.text)['lan']['macAddr']
     if len(json.loads(response.text)["wifi2Ghz"]["conn_client"]["conn_list"]) <= 1:
         print(f"💻 Connected Host: ", end="")
     else:
         print(f"💻 Connected Hosts: ", end="")
     print(", ".join(map(getHostname, json.loads(response.text)["wifi2Ghz"]["conn_client"]["conn_list"])))
-    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getWWanInfo"})
+    response = requests.post(URL, data={"page": "getWWanInfo"})
     uptime_hour = int(json.loads(response.text)['info']['h'])
     if uptime_hour > 8:
         print(colored(f"⏰ Uptime: {str(uptime_hour)}h {json.loads(response.text)['info']['m']}m {json.loads(response.text)['info']['s']}s", "red"))
@@ -72,7 +48,7 @@ if len(sys.argv) == 1:
     pub_ip = json.loads(response.text)['info']['public_ip']
     primary_dns = json.loads(response.text)['info']['primary_dns']
     sec_dns = json.loads(response.text)['info']['secondary_dns']
-    response = requests.post(URL, headers=headers, cookies=cookies, data={"page": "getIndicatorInfo"})
+    response = requests.post(URL, data={"page": "getIndicatorInfo"})
     battery = int(json.loads(response.text)['info']['disp_bat_per'])
     if int(json.loads(response.text)['info']['disp_bat_level']) == 5:
         print(colored("⚡️ Charging\n", "cyan"))
@@ -87,9 +63,9 @@ if len(sys.argv) == 1:
     print(f"📨 Public IP Addr: {pub_ip}")
     print(f"1️⃣  Primary DNS: {primary_dns}")
     print(f"2️⃣  Secondary DNS: {sec_dns}")
-    print(f"🔌 MAC Addr: {macAddr}")
+    print(f"🔌 MAC Addr: {macAddr}\n")
     end_time = time.process_time()
-    print(f"time elapsed : {int(round((end_time - start_time) * 1000))}ms")
+    print(f"⏳ Time Elapsed : {int(round((end_time - start_time) * 1000))}ms")
 
 else:
     if sys.argv[1] in ["-h", "--help"]:
